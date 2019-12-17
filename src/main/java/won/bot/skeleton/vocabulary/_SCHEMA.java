f963bf267c956c5bc43d0b2529749bdf96096c04
Samantha Fuchs <e1633020@student.tuwien.ac.at>
2019-12-17 14:28:04 +0100

    extend _SCHEMA

--- src/main/java/won/bot/skeleton/vocabulary/_SCHEMA.java
+++ src/main/java/won/bot/skeleton/vocabulary/_SCHEMA.java
@@ -12,11 +12,13 @@ public class _SCHEMA {
 
     public static final Property AUTHOR;
     public static final Property ISBN;
+    public static final Property URL;
     public static final Property BOOK_OFFER;
 
     static {
         AUTHOR = m.createProperty("http://schema.org/author");
         ISBN = m.createProperty("http://schema.org/isbn");
+        URL = m.createProperty("http://schema.org/url");
         BOOK_OFFER = m.createProperty("https://w3id.org/won/ext/demo#BookOffer");
     }
 }
